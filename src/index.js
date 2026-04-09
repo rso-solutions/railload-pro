43a73705f2c7b16f762c2e51da653fd36aa45df2
RSO Solutions <rob@rsosolutions.ca>
2026-04-08 20:15:37 -0600

    Add web console dashboard

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.json({ status: 'RailLoad Pro API running' });
